4e6ac1fdc8ac06e51f367e0deb95b908c94075db
albertopformoso <albertopformoso@gmail.com>
2021-03-03 18:16:58 -0600

    Flask Templates

---
+++ 005 Templates/02_index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Alberto"
+    letters = list(name)
+    pup_dictionary = {'pup_name': 'Sammy'}
+    return render_template('02_basic.html', 
+                           name=name, 
+                           letters=letters,
+                           pup_dictionary=pup_dictionary)
+
+if __name__ == '__main__':
+    app.run(debug=True)
