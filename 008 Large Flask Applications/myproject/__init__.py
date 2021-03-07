27d9ecd90d3245198d905633dd32520c8e03365a
albertopformoso <albertopformoso@gmail.com>
2021-03-07 17:29:33 -0600

    Large Flask Apps

---
+++ 008 Large Flask Applications/myproject/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv, find_dotenv
+
+
+app = Flask(__name__)
+
+# ---- .env check ---- 
+if len(find_dotenv()) == 0:
+        raise RuntimeError("Can't find your .env file")
+
+load_dotenv(find_dotenv())
+# --------------------
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# NOTE! These imports need to come after you've defined db, otherwise you will
+# get errors in your models.py files.
+## Grab the blueprints from the other views.py files for each "app"
+from myproject.puppies.views import puppies_blueprint
+from myproject.owners.views import owners_blueprint
+
+app.register_blueprint(owners_blueprint, url_prefix="/owners")
+app.register_blueprint(puppies_blueprint, url_prefix='/puppies')
